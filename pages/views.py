1837c0375571f7f37362043d94729005a562cb55
magnesiumh <93490427+magnesiumh@users.noreply.github.com>
2021-12-20 16:00:53 +0200

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -4,16 +4,16 @@ from django.views.generic import View
 from django.http import JsonResponse
 from django.http import HttpResponse
 # Create your views here.
-from .models import Page
+from .models import page
 
 
 def index(request, pagename):
     pagename = "/" + pagename
-    pg = get_object_or_404(Page, permalink=pagename)
+    pg = get_object_or_404(page, permalink=pagename)
     context = {'title': pg.title,
                'content': pg.bodytext,
                'last_updated': pg.update_date,
-               'page_list': Page.objects.all(), }
+               'page_list': page.objects.all(), }
     # assert False
     return render(request, 'pages/page.html', context)
 
