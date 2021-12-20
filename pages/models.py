4075d45ef9996e188069a0dab738d00802545a0f
magnesiumh <93490427+magnesiumh@users.noreply.github.com>
2021-12-20 11:24:23 +0200

    Update models.py

--- pages/models.py
+++ pages/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Page(models.Model):
+class page(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=60)
     permalink = models.CharField(max_length=12, unique=True)
