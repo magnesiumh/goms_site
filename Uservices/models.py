739504328100665a7ed629eb128e597fa3c6c257
magnesiumh <93490427+magnesiumh@users.noreply.github.com>
2021-12-20 09:07:41 +0200

    Update models.py

--- Uservices/models.py
+++ Uservices/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Vids(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=60)
     Embedded_link = models.CharField(max_length=1000, unique=True)
     Date_taken = models.DateTimeField()
