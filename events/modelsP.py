be67ae0004ae52ea61a129bf1e9043211629be34
magnesiumh <93490427+magnesiumh@users.noreply.github.com>
2021-12-20 09:11:11 +0200

    Update modelsP.py

--- events/modelsP.py
+++ events/modelsP.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Page(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=60)
     permalink = models.CharField(max_length=12, unique=True)
     update_date = models.DateTimeField('Last Updated')
